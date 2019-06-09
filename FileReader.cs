b14794b71e1c4c61d86994ddd24251e6f797d08c
dmykyten@gmail.com <dmykyten@gmail.com>
2019-06-09 13:43:30 +0300

    Added FileReader functionality

---
+++ FileReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BankOCR
+{
+    class FileReader
+    {
+        public string[] Lines { get; private set; }
+        public bool IsOkay { get; private set; }
+        public FileReader(string FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                Lines = File.ReadAllLines(FilePath);
+                for(int i = 0;i <= 2; i++)
+                {
+                    if (Lines[i].Length == 27)
+                    {
+                        IsOkay = true;
+                    }
+                    else
+                    {
+                        IsOkay = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                IsOkay = false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,33 @@ namespace BankOCR
     {
         static void Main(string[] args)
         {
+            T01_file_read();
+
         }
+        static void T01_file_read()
+        {
+            // valid file
+            var Reader = new FileReader(@"D:\Projects\Misc\Dmytryk\BankOCR\use-cases\123456789.txt");
+            Assert(Reader.IsOkay);
+            Assert(Reader.IsOkay == true);
+            Assert(Reader.Lines.Count() == 3);
+
+            // non existed file
+            Reader = new FileReader(@"D:\Projects\Misc\Dmytryk\BankOCR\ww.txt");
+            Assert(Reader.IsOkay == false);
+            Assert(Reader.Lines == null);
+
+            // not valid file
+            Reader = new FileReader(@"D:\Projects\Misc\Dmytryk\BankOCR\use-cases\invalid_length.txt");
+            Assert(Reader.IsOkay == false);
+            Assert(Reader.Lines.Length != 0);
+        }
+        static void Assert(bool Condition)
+        {
+            if (!Condition)
+            {
+                throw new Exception();
+            }
+        } 
     }
 }
